0f6ff245ae5f0499a0a8731e449e4c024d0fd4b1
hein <heinhtetaung1@my.jcu.edu.au>
2026-07-14 10:35:24 +0800

    Add homepage news feed

--- news/views.py
+++ news/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Article, Category
 
-# Create your views here.
+
+def home(request):
+    articles = Article.objects.select_related("source", "category").all()
+    categories = Category.objects.all()
+
+    context = {
+        "articles": articles,
+        "categories": categories,
+    }
+
+    return render(request, "news/home.html", context)
